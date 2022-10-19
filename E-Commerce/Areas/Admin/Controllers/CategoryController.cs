d32f45f23e9db65912f21a142e8fdfb866697589
tamji <tamji@DESKTOP-6GADVOU>
2022-10-19 16:07:28 +0600

    Add project files.

---
+++ E-Commerce/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,66 @@
+﻿using E_Commerce.Data;
+using E_Commerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.Categories.ToList());
+        }
+        //Create Get Action method
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //Create Post Action method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+        //Edit Get Action method
+        public ActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var category = _db.Categories.Find(id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        //Edit Post Action method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(category);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+    }
+}
---
+++ E-Commerce/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Category")]
+        public string CategoryName { get; set; }
+    }
+}
