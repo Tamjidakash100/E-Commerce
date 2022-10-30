efc6e2392fe975faf54cfa160fe9f522863b7c4f
Tamjid <tamjidakash100@gmail.com>
2022-10-30 13:34:15 +0600

    product create action is done

---
+++ E-Commerce/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using E_Commerce.Data;
+using E_Commerce.Models;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGeneration;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace E_Commerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private ApplicationDbContext _db;
+        private IHostingEnvironment _he;
+        public ProductsController(ApplicationDbContext db, IHostingEnvironment he)
+        {
+            _db=db;
+            _he=he;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.Products.Include(p=>p.Category).Include(t=>t.Tags).ToList());
+        }
+        //Create get action method
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //Create post action method
+        [HttpPost]
+        public async Task<IActionResult> Create(Products products, IFormFile image)
+        {
+            if(image!=null)
+            {
+                var name = Path.Combine(_he.WebRootPath+"/Images"+Path.GetFileName(image.FileName));
+                await image.CopyToAsync(new FileStream(name, FileMode.Create));
+                products.Image = "Images/"+ image.FileName;
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Products.Add(products);
+                await _db.SaveChangesAsync();
+                return RedirectToAction (nameof(Index));
+            }
+            return View(products);
+        }
+    }
+}
--- E-Commerce/Data/ApplicationDbContext.cs
+++ E-Commerce/Data/ApplicationDbContext.cs
@@ -12,5 +12,6 @@ namespace E_Commerce.Data
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Tags> Tags { get; set; }
+        public DbSet<Products> Products { get; set; }
     }
 }
