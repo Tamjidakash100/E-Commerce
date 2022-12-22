d09d47dde03782bcff4c978f4b2a8c57fa91f87a
Tamjid <tamjidakash100@gmail.com>
2022-12-22 15:06:50 +0600

    Role assign complete

---
+++ E-Commerce/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,148 @@
+﻿using E_Commerce.Areas.Admin.Models;
+using E_Commerce.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace E_Commerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RoleController : Controller
+    {
+        UserManager<IdentityUser> _userManager;
+        RoleManager<IdentityRole> _rolemanager;
+        ApplicationDbContext _db;
+        public RoleController(ApplicationDbContext db, RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
+        {
+            _db=db;
+            _rolemanager = roleManager;
+            _userManager = userManager;
+        }
+        //Get Index Action Method
+        public IActionResult Index()
+        {
+            var roles = _rolemanager.Roles.ToList();
+            ViewBag.Roles=roles;
+            return View(ViewBag.Roles);
+        }
+        //Get Create Action Method
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //Post Create Action Method
+        [HttpPost]
+        public async Task<IActionResult> Create(string name)
+        {
+            bool isexist = await _rolemanager.RoleExistsAsync(name);
+            if (isexist)
+            {
+                ViewBag.message= "Role Name Already Exists!!!!";
+                ViewBag.name= name;
+                return View();
+            }
+            IdentityRole role= new IdentityRole();
+            role.Name= name;
+            var result = await _rolemanager.CreateAsync(role);
+            if (result.Succeeded)
+            {
+                TempData["Save"]= "Role Is Saved Successfully";
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        //Get Edit Action Method
+        public async Task<IActionResult> Edit(string id)
+        {
+            var role= await _rolemanager.FindByIdAsync(id);
+            if(role==null)
+            {
+                return NotFound();
+            }
+            ViewBag.id=role.Id;
+            ViewBag.name=role.Name;
+
+            return View();
+        }
+        //Post Edit Action Method
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, string name)
+        {
+            var role = await _rolemanager.FindByIdAsync(id);
+            if (role==null)
+            {
+                return NotFound();
+            }
+            bool isexist = await _rolemanager.RoleExistsAsync(name);
+            if (isexist)
+            {
+                ViewBag.message= "Role Name Already Exists!!!!";
+                ViewBag.name= name;
+                ViewBag.id= id;
+
+                return View();
+            }
+            role.Name= name;
+            var result = await _rolemanager.UpdateAsync(role);
+            if (result.Succeeded)
+            {
+                TempData["Edit"]= "Role Has been Updated Successfully";
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        //Get Delete Action Method
+        public async Task<IActionResult> Delete(string id)
+        {
+            var role = await _rolemanager.FindByIdAsync(id);
+            if (role==null)
+            {
+                return NotFound();
+            }
+            ViewBag.id=role.Id;
+            ViewBag.name=role.Name;
+
+            return View();
+        }
+        //Post Delete Action Method
+        [HttpPost]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirm(string id)
+        {
+            var role = await _rolemanager.FindByIdAsync(id);
+            if (role==null)
+            {
+                return NotFound();
+            }
+            var result = await _rolemanager.DeleteAsync(role);
+            if (result.Succeeded)
+            {
+                TempData["Delete"]= "Role Has been Deleted Successfully";
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Assign()
+        {
+            ViewData["UserId"] = new SelectList(_db.ApplicationUsers.ToList(),"Id","UserName");
+            ViewData["RoleId"] = new SelectList(_rolemanager.Roles.ToList(),"Name","Name");
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Assign(AssignRolesVM assignRoles)
+        {
+            var user = _db.ApplicationUsers.FirstOrDefault(c => c.Id==assignRoles.UserId);
+            bool exist = await _userManager.IsInRoleAsync(user, assignRoles.RoleId);
+            if (exist)
+            {
+                ViewBag.message = "This role is already assigned to this user";
+                return View();
+            }
+
+            var role = await _userManager.AddToRoleAsync(user,assignRoles.RoleId);
+            if (role.Succeeded)
+            {
+                TempData["Save"]="User Role has been assigned";
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ E-Commerce/Areas/Admin/Models/AssignRolesVM.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace E_Commerce.Areas.Admin.Models
+{
+    public class AssignRolesVM
+    {
+        [Required]
+        [Display(Name ="Role")]
+        public string RoleId { get; set; }
+        [Required]
+        [Display(Name = "User")]
+        public string UserId { get; set; }
+    }
+}
